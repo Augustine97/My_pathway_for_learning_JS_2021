ca95b42a4888e554b3a5694e2d2de0bf68819bbe
Augustine97 <manu71997mathew@gmail.com>
2021-01-10 03:34:45 +0530

    Starting JS and added I am going to rock the Java script mangit statusgit status! in index.js file

---
+++ JS/index.js
@@ -0,0 +1,1 @@
+console.log("I am going to rock the Java script man!!!!!");
---
+++ hello.js
@@ -0,0 +1,2 @@
+console.log("---------------------########Hello Manu!!! So U are finally into JS:)");
+console.log("---------------------########Hii Js!!! I am loving this");
