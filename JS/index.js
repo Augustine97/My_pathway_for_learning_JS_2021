923ffe5e404bc09fea58f6992977d9e478b3a5a9
Augustine97 <manu71997mathew@gmail.com>
2021-01-10 16:38:30 +0530

    Constants Done

--- JS/index.js
+++ JS/index.js
@@ -17,3 +17,11 @@ console.log(my_name);
 let firstName = 'Manu';
 let lastName = 'Mathew';
 console.log(firstName +" "+lastName);
+
+// modern way to declare constants 
+const interestrate = 0.3;
+// The line below is commented to remove errors.
+// interestrate = 1;
+console.log(interestrate);
+// if we execute the above code from linenos(22,23,24) we get the error index.js:23 "Uncaught TypeError: Assignment to constant variable. at index.js:23"
+// if we need to re-assign a variable  again and again use "let" else use "const"
