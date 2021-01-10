3a59c2531a1ccd8c04f089dfc23618e5a5950199
Augustine97 <manu71997mathew@gmail.com>
2021-01-10 13:11:48 +0530

    added rules for declaring variables in index.js

--- JS/index.js
+++ JS/index.js
@@ -3,3 +3,10 @@ console.log("I am going to rock the Java script man!!!!!");
 // declaration of variables
 let my_name = "Manu Mathew";
 console.log(my_name);
+
+// Rules of declaring variable declaration
+// 1. Cannot be a reserved keyword.
+// 2. Should be meaningful names.
+// 3. Cannot start with a number (1name).
+// 4.Cannot contain a space or hyphen(-).
+// 5. use camel notation if possible.
